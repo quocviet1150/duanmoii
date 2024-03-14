0df1548f6b76d4c74918544848b7a9d1870910b0
vietnq <vietnq@nissho-vn.com>
2024-03-14 17:21:41 +0700

    commit

---
+++ login_WPF/CustomControls/BindablePassword.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace login_WPF.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BindablePassword.xaml
+    /// </summary>
+    public partial class BindablePassword : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty =
+           DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePassword));
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+
+        public BindablePassword()
+        {
+            InitializeComponent();
+            txtPassWord.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = txtPassWord.SecurePassword;
+        }
+    }
+}
---
+++ login_WPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace login_WPF.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible=true;
+
+        public string Username {
+            get
+            {
+                return _username;
+            }
+            set { 
+                _username = value; 
+                OnPropertyChanged(nameof(Username));
+            }
+                }
+        public SecureString Password {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible {
+            get
+            {
+                return _isViewVisible;
+            }
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        public ICommand LoginCommand {  get;}
+        public ICommand RecoverPasswordCommand {  get;}
+        public ICommand ShowPasswordCommand {  get;}
+        public ICommand RememberPasswordCommand {  get;}
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverCommand("", ""));
+        }
+
+        private void ExecuteRecoverCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+
+            if (string.IsNullOrEmpty(Username) || Username.Length < 3 ||
+                Password == null || Password.Length < 3)
+            {
+                validData = false;
+            }
+            else
+            {
+                validData = true;
+            }
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
